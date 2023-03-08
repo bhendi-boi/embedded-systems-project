7d346b4cd4a199fbc8138c37b00fed1ae5db81bb
bhendi-boi <bhendiisgreen@gmail.com>
2023-03-08 14:57:12 +0530

    added few resources basic hello world! is displayed on OLED screen now

--- readme.md
+++ readme.md
@@ -7,3 +7,7 @@ by [Jyothikrishna](https://github.com/bhendi-boi) and [Advaita Kartikeya](https:
 - take analog input from joysticks
 - write to led screen
 - find similar proj on github
+
+## Resources
+
+- [Interfacing OLED display with Arduino](https://arduinogetstarted.com/tutorials/arduino-oled)
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width,  in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// declare an SSD1306 display object connected to I2C
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup()
 {
   // put your setup code here, to run once:
-  pinMode(DD7, OUTPUT);
+
+  Serial.begin(9600);
+  // initialize OLED display with address 0x3C for 128x64
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true)
+      ;
+  }
+
+  delay(2000);         // wait for initializing
+  oled.clearDisplay(); // clear display
+
+  oled.setTextSize(1);          // text size
+  oled.setTextColor(WHITE);     // text color
+  oled.setCursor(0, 10);        // position to display
+  oled.println("Hello World!"); // text to display
+  oled.display();               // show on OLED
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  digitalWrite(DD7, HIGH);
-  delay(1000);
-  digitalWrite(DD7, LOW);
-  delay(1000);
 }
